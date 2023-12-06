92e47536811b2345fcef81bfa9101a2517c0b5d5
Sana Rashid <sanarashid7204@gmail.com>
2023-12-06 21:47:02 +0500

    Update comments

--- bakers_app/lib/Pages/home_page.dart
+++ bakers_app/lib/Pages/home_page.dart
@@ -83,6 +83,7 @@ class _HomePageState extends State<HomePage> {
                           Navigator.push(
                               context,
                               MaterialPageRoute(
+                                  //route to detail page of selected item
                                   builder: (builder) =>
                                       DetailPage(cake: cakes[index])));
                         },
